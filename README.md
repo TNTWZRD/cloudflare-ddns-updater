d828bfb2f1acf70af48d4e29dc0042fe12e7eccf
root <root@pve.lan>
2021-07-21 15:11:29 -0400

    Inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# cloudflare-ddns-updater
-Cloudflare DNS public IPV4 Updater, Multiple Zones
+# Cloudflare Dynamic DNS Multiple IP Updater
+
+This script is used to update Multiple dynamic DNS entries for accounts on Cloudflare.
+
+## Installation
+
+```bash
+git clone https://github.com/TNTWZRD/cloudflare-ddns-updater
+```
+
+## Usage
+This script is used with crontab. Specify the frequency of execution through crontab.
+
+```bash
+# Recommended once per day
+# * 0 * * * /bin/bash pyhton3 {Location of the script}
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Reference
+This script was made with reference from [K0p1-Git]https://github.com/K0p1-Git/cloudflare-ddns-updater
---
+++ cloudflare-update.py
@@ -0,0 +1,45 @@
+import requests
+import sys
+import json
+
+## Load Pertinate Files
+Secret = json.load(open("secret.json"))
+ZoneList = json.load(open("zonelist.json"))
+
+# Create Headers
+headers = {'content-type': 'application/json', 'Authorization': 'Bearer '+Secret["DNSAPI-TOKEN"]}
+
+# ## Get IP
+IP = requests.get("https://api.ipify.org").text
+
+## Make Sure we have a valid IP
+if IP == "":
+    sys.exit("No Public IP cant update!")
+else:
+    print("Public IP Found: "+IP)
+
+## Create dictionary to fill with records that need updating
+toUpdate = []
+
+## Loop through each zone, and each domain in that zone to see if they need updating
+for ZoneIdentifier in ZoneList:
+    for RecordName in ZoneList[ZoneIdentifier]:
+        print("Checking " + ZoneIdentifier + " : " + RecordName)
+        record = requests.get("https://api.cloudflare.com/client/v4/zones/"+ZoneIdentifier+"/dns_records?name="+RecordName, headers=headers).json()
+        if(record["result_info"]["count"] == 0):
+            print(ZoneIdentifier + " : " + RecordName + " || Invalid Record, Check spelling or create record")
+            break
+        for result in record["result"]:
+            if(result["type"] != "A"): break
+            if(result["content"] != IP):
+                toUpdate.append({"ZoneID": result["zone_id"], "RecordID": result["id"], "Proxy": result["proxied"], "RecordName": result["name"]})
+
+## Exit if all records are current
+if(len(toUpdate) == 0): sys.exit("All records current, No updating required!")
+
+# Update Records with new IP
+for record in toUpdate:
+    data = {"type":"A","name":record["RecordName"],"content":IP,"ttl":1,"proxied":record["Proxy"]}
+    update = requests.put("https://api.cloudflare.com/client/v4/zones/" + record["ZoneID"] + "/dns_records/" + record["RecordID"], data = json.dumps(data), headers=headers).json()
+    if(update["success"]): print("Updated "+record["RecordName"]+" With new IP: "+ IP)
+    else: print("ERROR: " + update)
